c2465f2347425f3f96fdb726f7440fec03179b68
maanpatel <168857286+mffgbdojvfw@users.noreply.github.com>
2025-06-26 21:26:03 +0530

    Update connection.js

--- backend/connection/connection.js
+++ backend/connection/connection.js
@@ -1,14 +1,17 @@
-// config/connection.js
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config(); // Load .env variables
 
-const connectDB = async () => {
+const connect = async () => {
   try {
-    const conn = await mongoose.connect("mongodb://127.0.0.1:27017/Excel_Analytics");
-    console.log(` MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error(`MongoDB connection failed: ${error.message}`);
-    process.exit(1); // Exit the process if DB connection fails
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log("Connected successfully to MongoDB Atlas!");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
   }
 };
 
-module.exports = connectDB;
+module.exports = connect;
